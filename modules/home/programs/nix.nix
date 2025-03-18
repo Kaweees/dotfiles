65b657d5e1fbe92f6b0c1c154d01eab2b91186b9
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-03-17 18:39:28 -0700

    fix: Update Nix package names

--- modules/home/programs/nix.nix
+++ modules/home/programs/nix.nix
@@ -1,12 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    nix-instantiate
-    nixfmt
-    nix-build
-    nix-env
-    nix-channel
-    nix-collect-garbage
-    nix-daemon
-    nix-env
+    nixpkgs-fmt
+    nix
   ];
 }
