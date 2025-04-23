7f73b231670c9d245cdd5d067f165142849968ee
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-04-23 10:44:25 -0700

    lib

---
+++ lib/colmenaSystem.nix
@@ -0,0 +1,40 @@
+# colmena - Remote Deployment via SSH
+{
+  lib,
+  inputs,
+  nixos-modules,
+  home-modules ? [],
+  myvars,
+  system,
+  tags,
+  ssh-user,
+  genSpecialArgs,
+  specialArgs ? (genSpecialArgs system),
+  ...
+}: let
+  inherit (inputs) home-manager;
+in
+  {name, ...}: {
+    deployment = {
+      inherit tags;
+      targetUser = ssh-user;
+      targetHost = name; # hostName or IP address
+    };
+
+    imports =
+      nixos-modules
+      ++ (
+        lib.optionals ((lib.lists.length home-modules) > 0)
+        [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "home-manager.backup";
+
+            home-manager.extraSpecialArgs = specialArgs;
+            home-manager.users."${myvars.username}".imports = home-modules;
+          }
+        ]
+      );
+  }
